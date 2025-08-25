8408ae54c9681c93ff2551c55d824a788d543752
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-25 17:40:26 -0600

    feat: crea elemento de card principal

---
+++ src/components/mainContent/allContent/mainContent/MainCard.tsx
@@ -0,0 +1,22 @@
+import PlayGreenMain from "@/icons/PlayGreenMain";
+
+interface Playlist {
+    id: string;
+    title: string;
+    cover?: string;
+}
+
+
+const MainCard = ({playlist}: {playlist: Playlist}) => {
+  return (
+    <article className="gap-2 overflow-hidden relative group w-40 rounded-sm mt-1 hover:bg-zinc-800 px-2 py-4 cursor-pointer transition-all duration-150 ease-in-out">
+        <figure className="size-[144px]">
+            <img className="w-full h-full rounded-sm" src={playlist.cover} alt={playlist.title} />
+            <PlayGreenMain />
+        </figure>
+        <p className="text-zinc-400 text-sm line-clamp-2 mt-2">{playlist.title}</p>
+    </article>
+  )
+}
+
+export default MainCard
