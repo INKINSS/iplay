1b45aceb460b2c729a12d68f6342ba2b6ac1248b
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-25 17:41:24 -0600

    feat(icons and image): crea iconos para cards principales

---
+++ src/icons/PlayGreenMain.tsx
@@ -0,0 +1,20 @@
+
+const PlayGreenMain = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="black"
+        viewBox="0 0 24 24"
+        className="size-10 absolute bg-green-400 rounded-full p-2 bottom-[3.7rem] group-hover:bottom-[4.7rem] right-4 opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out shadow-xl/30"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+        />
+      </svg>
+    )
+  }
+  
+  export default PlayGreenMain
+  
