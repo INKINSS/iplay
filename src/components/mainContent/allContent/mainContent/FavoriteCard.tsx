2afc0987cdb20ecac7e34ffe2789ee19324397c4
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-25 17:38:01 -0600

    feat: crea la card de favoritos para el header de la vista principal

---
+++ src/components/mainContent/allContent/mainContent/FavoriteCard.tsx
@@ -0,0 +1,36 @@
+import PlayGreen from "@/icons/PlayGreen";
+
+interface Playlist {
+    id?: string;
+    title?: string;
+    color?: string;
+    cover?: string;
+  }
+
+const FavoriteCard = ({ playlist }: { playlist: Playlist }) => {
+    return (
+        <article
+        className="text-white flex items-center max-w-[250px]"
+        style={{ backgroundColor: playlist.color }}
+      >
+        <div className="flex items-center gap-2 group justify-center bg-whiteOpacityInactive hover:bg-whiteOpacityActive transition-all duration-150 ease-in-out mt-6 w-full overflow-hidden pr-3 cursor-pointer rounded-sm relative">
+          {playlist.cover && (
+            <figure className="size-14 flex-shrink-0">
+              <img
+                src={playlist.cover}
+                alt={playlist.title}
+                className="w-full h-full object-cover"
+              />
+              <PlayGreen />
+            </figure>
+          )}
+          <h3 className="text-sm font-medium truncate whitespace-nowrap overflow-hidden">
+            {playlist.title}
+          </h3>
+        </div>
+      </article>
+      
+    );
+  }
+  
+export default FavoriteCard
