bb7030fada3bb655f61d71342e055996df5b4839
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-25 17:39:32 -0600

    feat: crea componente principal de la vista "todo"

---
+++ src/components/mainContent/allContent/mainContent/IndexContent.tsx
@@ -0,0 +1,13 @@
+import HeaderContent from "./HeaderContent";
+import PersonalContent from "./PersonalContent";
+
+const IndexContent = () => {
+  return (
+    <section className="space-y-6">
+      <HeaderContent />
+      <PersonalContent />
+    </section>
+  );
+};
+
+export default IndexContent;
