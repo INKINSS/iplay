f6dec43ff03806fc3ba3877475af16eaac7ea400
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-25 17:40:01 -0600

    feat: integra elemento principal en la vista "todo"

--- src/components/mainContent/MainContent.tsx
+++ src/components/mainContent/MainContent.tsx
@@ -1,17 +1,18 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs"
+import IndexContent from "./allContent/mainContent/IndexContent"
 
 
 const MainContent = () => {
   return (
-    <main className="py-4 px-8">
+    <main className="py-4 px-8 w-full">
         <div>
-        <Tabs defaultValue="todo" className="w-[400px]">
+        <Tabs defaultValue="todo" className="w-full">
   <TabsList className="flex gap-2 text-[.8rem]">
-    <TabsTrigger className="data-[state=active]:text-black data-[state=inactive]:bg-whiteOpacityInactive data-[state=inactive]:text-white bg-whiteOpacity rounded-full px-3 py-2" value="todo">Todo</TabsTrigger>
-    <TabsTrigger className="data-[state=active]:text-black data-[state=inactive]:bg-whiteOpacityInactive data-[state=inactive]:text-white bg-whiteOpacity rounded-full px-3 py-2" value="music">Música</TabsTrigger>
-    <TabsTrigger className="data-[state=active]:text-black data-[state=inactive]:bg-whiteOpacityInactive data-[state=inactive]:text-white bg-whiteOpacity rounded-full px-3 py-2" value="podcasts">Podcasts</TabsTrigger>
+    <TabsTrigger className="data-[state=active]:text-black data-[state=inactive]:bg-whiteOpacityInactive data-[state=inactive]:text-white bg-whiteOpacity rounded-full px-3 py-2 cursor-pointer" value="todo">Todo</TabsTrigger>
+    <TabsTrigger className="data-[state=active]:text-black data-[state=inactive]:bg-whiteOpacityInactive data-[state=inactive]:text-white bg-whiteOpacity rounded-full px-3 py-2 cursor-pointer" value="music">Música</TabsTrigger>
+    <TabsTrigger className="data-[state=active]:text-black data-[state=inactive]:bg-whiteOpacityInactive data-[state=inactive]:text-white bg-whiteOpacity rounded-full px-3 py-2 cursor-pointer" value="podcasts">Podcasts</TabsTrigger>
   </TabsList>
-  <TabsContent value="todo">Todo</TabsContent>
+  <TabsContent className="w-full" value="todo"><IndexContent /></TabsContent>
   <TabsContent value="music">Música</TabsContent>
   <TabsContent value="podcasts">Podcasts</TabsContent>
 </Tabs>
