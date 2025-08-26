750892ecaf809c9aafb2171477d64424b3f3402a
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-26 10:22:03 -0600

    feat: muestra el apartado de albumes publicos en la seccion principal

--- src/components/mainContent/allContent/mainContent/IndexContent.tsx
+++ src/components/mainContent/allContent/mainContent/IndexContent.tsx
@@ -1,11 +1,13 @@
 import HeaderContent from "./HeaderContent";
 import PersonalContent from "./PersonalContent";
+import PublicSong from "./PublicSong";
 
 const IndexContent = () => {
   return (
     <section className="space-y-6">
       <HeaderContent />
       <PersonalContent />
+      <PublicSong />
     </section>
   );
 };
