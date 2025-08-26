2b107d27687ddcfafc4c044aeb155a50bfb6a2a9
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-08-26 10:24:29 -0600

    feat(data): crea y devuelve un album en el array de canciones

---
+++ src/utils/getAlbums.ts
@@ -0,0 +1,26 @@
+// utils/getAlbums.ts
+import { songs } from "@/lib/data";
+
+export interface Album {
+  id: number;
+  title: string;
+  image: string;
+  artists: string[];
+}
+
+export const getAlbums = (): Album[] => {
+  const albumsMap = new Map<number, Album>();
+
+  songs.forEach((song) => {
+    if (!albumsMap.has(song.albumId)) {
+      albumsMap.set(song.albumId, {
+        id: song.albumId,
+        title: song.album,
+        image: song.image,
+        artists: song.artists,
+      });
+    }
+  });
+
+  return Array.from(albumsMap.values());
+};
