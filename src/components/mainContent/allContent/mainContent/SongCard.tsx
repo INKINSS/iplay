8fc73a29414fc08cbf332c1f698111b407fd9f14
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-09-01 09:25:13 -0600

    feat: renderiza dinamicamente el boton de estado dependiendo del estado global

--- src/components/mainContent/allContent/mainContent/SongCard.tsx
+++ src/components/mainContent/allContent/mainContent/SongCard.tsx
@@ -1,4 +1,6 @@
+import PauseGreenSongs from "@/icons/PauseGreenSongs";
 import PlayGreenSongs from "@/icons/PlayGreenSongs";
+import { usePlayerStore } from "@/store/playerStore";
 
 interface Song {
     id: string | number;
@@ -9,11 +11,20 @@ interface Song {
 
 
 const SongCard = ({song}: {song: Song}) => {
+    const { isPlaying, currentMusic, setCurrentMusic } = usePlayerStore(state => state);
+    const currentPlaylist = isPlaying && currentMusic?.song?.id === song.id;
+
+    const handleClick = () => {
+        setCurrentMusic({playlist:{
+            id: song.id,
+        }, song, songs: []});
+    }
+    
   return (
-    <a href={`/album/${song.id}`} className="flex-shrink-0 relative gap-2 group w-40 rounded-sm hover:bg-zinc-800 px-2 py-4 cursor-pointer transition-all duration-150 ease-in-out">
+    <a href={`/album/${song.id}`} className="flex-shrink-0 relative gap-2 group w-40 rounded-sm hover:bg-zinc-800 px-2 py-4 cursor-pointer transition-all duration-150 ease-in-out" onClick={handleClick}>
         <figure className="size-[144px]">
             <img className="w-full h-full rounded-sm" src={song.image} alt={song.title} />
-            <PlayGreenSongs />
+            {currentPlaylist ? <PauseGreenSongs /> : <PlayGreenSongs />}
         </figure>
         <div className="flex flex-col">
             <p className="text-zinc-100 text-[1rem] font-medium line-clamp-2 mt-2 leading-5">{song.title}</p>
