8a5a0fedb583e5aa8f6b7949a1bed7321bc912c6
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-09-05 11:27:24 -0600

    feat(icons and image): añade estilos para elemento de play para cada podcast

---
+++ src/components/podcast/PlayPodcast.tsx
@@ -0,0 +1,11 @@
+import PlayBlack from "@/icons/PlayBlack"
+
+const PlayPodcast = () => {
+  return (
+    <figure className="size-10 aspect-square flex items-center justify-center rounded-full bg-white p-3">
+        <PlayBlack />
+    </figure>
+  )
+}
+
+export default PlayPodcast
