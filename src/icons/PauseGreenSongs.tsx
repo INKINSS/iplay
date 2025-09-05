899a3251d235da47584572ebf91f3bf546bf873d
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-09-05 09:34:37 -0600

    style: añade estilos para boton verde de canciones independientes

--- src/icons/PauseGreenSongs.tsx
+++ src/icons/PauseGreenSongs.tsx
@@ -1,8 +1,7 @@
 
 const PauseGreenSongs = () => {
   return (
-    <svg data-encore-id="icon" role="img" aria-hidden="true"         className="size-10 absolute bg-green-400 rounded-full p-2 bottom-[4.9rem] group-hover:bottom-[6rem] right-4 opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out sha"
-    viewBox="0 0 24 24"><path d="M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7z"></path></svg>
+    <svg data-encore-id="icon" role="img" aria-hidden="true" className="size-10 absolute bg-green-400 rounded-full p-2 bottom-[4.9rem] group-hover:bottom-[6rem] right-4 opacity-0 group-hover:opacity-100 transition-all duration-200 ease-in-out sha" viewBox="0 0 24 24"><path d="M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7z"></path></svg>
   )
 }
 
