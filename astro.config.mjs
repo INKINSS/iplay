339c53a72b09d74127343e161a06ee22c232e041
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-09-05 14:37:11 -0600

    feat: añade vercel como adaptador serveless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-    },
-    output: 'server',
+  devToolbar: {
+      enabled: false
+  },
+  output: 'server',
+  adapter: vercel({}),
   vite: {
       plugins: [tailwind()],
   },
-
   integrations: [react()],
 });
